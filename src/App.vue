f446c507b6f5327c2e37084de4bfe77775eabad3
lkgjelst <lkgjelst@stud.ntnu.no>
2020-03-23 19:06:35 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <h1> Heisann </h1>
+    <div class="chart">
+      <LineChart/>
+    </div>
+  </div>
+</template>
+
+<script>
+import LineChart from './components/LineChart.vue'
+
+export default {
+  name: 'App',
+  components: {
+    LineChart,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+#chart {
+margin: 60px;
+color: #2180ce;
+height: 200px;
+}
+</style>
---
+++ src/components/LineChart.vue
@@ -0,0 +1,25 @@
+<!-- 
+Linechart using Graph.js for Vue
+-->
+
+<script>
+import { Line } from 'vue-chartjs'
+
+export default {
+    extends: Line,
+    data: function() {
+        return{
+            labels: ['Tested', 'Positive', 'Deaths'],
+            chartdata: [40,20,30]
+        };
+    },
+    mounted () {
+        this.renderChart(this.chartdata, this.options)
+    }
+    
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
